f355f9d3ee84b778dbe51e119ba68f5fadc97f88
vidushij11 <vidushij078@gmail.com>
2024-09-16 02:28:07 +0530

    access granted

--- app/main.py
+++ app/main.py
@@ -2,5 +2,11 @@ from fastapi import FastAPI
 from app.api.api_v1.api import api_router
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(api_router, prefix="/api/v1")
